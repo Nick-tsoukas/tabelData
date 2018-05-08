8c40d907e3b4f3e13197d7b6a475be68546305db
Nick Tsoukas <nick.tsoukas101@gmail.com>
2018-05-08 16:41:48 -0600

    Updating code

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-// em
-//role
-// start_date
-// m_rate
-// sub = button
+// at some point I will clear the Database because there is a ton of lkjsdflkjsd in it
 
 var config = {
   apiKey: "AIzaSyAoxi4Mk0vWYUCfwgERQjpmk7SFYrOx3x4",
@@ -12,24 +8,28 @@ var config = {
   storageBucket: "employee-cb45d.appspot.com",
   messagingSenderId: "298666775692"
 };
+
 firebase.initializeApp(config);
 var database = firebase.database();
 
+// setting the variables that we need to empty strings
 var em = "";
 var role = "";
 var start_date = "";
 var m_rate = "";
 
+
+//this is our click event: when user submits buttons
 $("#sub").on('click', function(e) {
   e.preventDefault();
 
-
+//getting the value from the form ===================
  em = $("#em").val().trim();
  role = $("#role").val().trim();
  start_date = $("#start_date").val().trim();
  m_rate = $("#m_rate").val().trim();
- console.log(em)
 
+// pushing form data to the firebase database
     database.ref().push({
       name: em,
       role: role,
@@ -38,94 +38,26 @@ $("#sub").on('click', function(e) {
     });
   });
 
-  // var template =
-  //               `
-  //               <tr>
-  //                 <td>${}</td>
-  //                 <td>${em}</td>
-  //                 <td>${em}</td>
-  //                 <td>${em}</td>
-  //                 <td>${em}</td>
-  //                 <td>${em}</td>
-  //               </tr>
-  //               `;
 
-// $("#myRow").append(database.name);
-
+// In the event that data is added to the database via: push({}) run this code
 database.ref().on("child_added", function(snapshot) {
-  console.log(snapshot.val().name)
+// This is the template literal you can lean more at : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+
+// we build the entire HTML and then interpolate the ===snapshot.val().name=== with ${some js code or vars}
   var template =
                 `
                 <tr>
                   <td>${snapshot.val().name}</td>
-                  <td>${snapshot.val().name}</td>
-                  <td>${snapshot.val().name}</td>
-                  <td>${snapshot.val().name}</td>
-                  <td>${snapshot.val().name}</td>
-                  <td>${snapshot.val().name}</td>
+                  <td>${snapshot.val().role}</td>
+                  <td>${snapshot.val().start_date}</td>
+                  <td>Must build function</td>
+                  <td>${snapshot.val().m_rate}</td>
+                  <td>Must build function</td>
                 </tr>
+
                 `;
+    //just logging the template
+    console.log(template)
+    // we now append the template literal to the DOM/HTML
     $("#myRow").append(template);
 });
-
-
-
-
-// var config = {
-//     apiKey: "AIzaSyA5eYKsB8T2q6rMGdKSvac6eQsWTzsZEjE",
-//     authDomain: "fir-recent-user.firebaseapp.com",
-//     databaseURL: "https://fir-recent-user.firebaseio.com",
-//     storageBucket: "fir-recent-user.appspot.com"
-//   };
-//
-//   firebase.initializeApp(config);
-//
-//   // Create a variable to reference the database.
-//   var database = firebase.database();
-//
-//   // Initial Values
-//   var name = "";
-//   var email = "";
-//   var age = 0;
-//   var comment = "";
-//
-//   // Capture Button Click
-//   $("#add-user").on("click", function(event) {
-//     event.preventDefault();
-//
-//     // Grabbed values from text-boxes
-//     name = $("#name-input").val().trim();
-//     email = $("#email-input").val().trim();
-//     age = $("#age-input").val().trim();
-//     comment = $("#comment-input").val().trim();
-//
-//     // Code for "Setting values in the database"
-//     database.ref().set({
-//       name: name,
-//       email: email,
-//       age: age,
-//       comment: comment
-//     });
-//
-//   });
-//
-//   // Firebase watcher + initial loader HINT: .on("value")
-//   database.ref().on("value", function(snapshot) {
-//
-//     // Log everything that's coming out of snapshot
-//     console.log(snapshot.val());
-//     console.log(snapshot.val().name);
-//     console.log(snapshot.val().email);
-//     console.log(snapshot.val().age);
-//     console.log(snapshot.val().comment);
-//
-//     // Change the HTML to reflect
-//     $("#name-display").text(snapshot.val().name);
-//     $("#email-display").text(snapshot.val().email);
-//     $("#age-display").text(snapshot.val().age);
-//     $("#comment-display").text(snapshot.val().comment);
-//
-//     // Handle the errors
-//   }, function(errorObject) {
-//     console.log("Errors handled: " + errorObject.code);
-//   });
